f196d951a6c08ba7d7809cc58b2f8e819f572a28
Graham Smith <grahamas@gmail.com>
2019-04-09 08:35:59 -0500

    [WIP] debugging docs deployment

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,10 @@ using Pkg; Pkg.activate(pwd())
 
 using Documenter, Simulation73
 
-makedocs(sitename="Simulation73 Documentation")
+makedocs(
+    sitename="Simulation73 Documentation",
+    pages=["index.md"]
+)
 
 deploydocs(
     repo = "github.com/grahamas/Simulation73.git"
