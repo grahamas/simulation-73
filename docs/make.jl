e0646f0e4af6b7aa5c5fbb9e65571e7682a5c9f6
Graham Smith <grahamas@gmail.com>
2019-04-10 11:17:41 -0500

    Hide internal docs from sidebar

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,27 @@ using Documenter, Simulation73
 
 makedocs(
     sitename="Simulation73 Documentation",
-    modules=[Simulation73]
+    modules=[Simulation73],
+    pages = [
+       "Home" => "index.md",
+       # "Manual" => Any[
+       #     "Guide" => "man/guide.md",
+       #     "man/examples.md",
+       #     "man/syntax.md",
+       #     "man/doctests.md",
+       #     "man/latex.md",
+       #     hide("man/hosting.md", [
+       #         "man/hosting/walkthrough.md"
+       #     ]),
+       #     "man/other-formats.md",
+       # ],
+       "Library" => Any[
+           "Public" => "lib/public.md",
+           hide("Internals" => "lib/internals.md",
+           Any[joinpath("lib/internals", f) for f in readdir("docs/src/lib/internals")]
+           )
+       ]
+   ]
 )
 
 deploydocs(
