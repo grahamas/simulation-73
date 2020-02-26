ead20697a102e6501f881023138ab1e9d3545a0a
Graham Smith <grahams@uchicago.edu>
2020-02-26 16:11:03 -0600

    Line subsampling

--- src/subsampling.jl
+++ src/subsampling.jl
@@ -6,6 +6,15 @@
 # Optional, allow specifying "tstops" which forces non-interpolation.
 
 abstract type AbstractSubsampler{D} end
+function subsample(obj, sub_arr::AbstractArray)
+    for sub in sub_arr
+        obj = subsample(obj, sub)
+    end
+    return obj
+end
+function subsample(lattice::LATTICE, sub::AbstractSubsampler{D}) where {T,D,LATTICE<:AbstractLattice{T,D}}
+    LATTICE(lattice.arr[sub])
+end
 subsample(space::AbstractSpace, ::Nothing) = space
 
 @with_kw struct IndexSubsampler{D} <: AbstractSubsampler{D}
@@ -14,8 +23,8 @@ end
 @with_kw struct ValueSubsampler{D,T} <: AbstractSubsampler{D}
 	Δ::NTuple{D,T}
 end
-@with_kw struct ValueWindower{D,T} <: AbstractSubsampler{D}
-	window::NTuple{D,T}
+@with_kw struct RightCutFrom{D,T} <: AbstractSubsampler{D}
+	cut::NTuple{D,T}
 end
 struct RadialSlice <: AbstractSubsampler{2} end
 "Subsample a space down to a new space"
@@ -41,6 +50,11 @@ struct StrideToEnd
 	start::Int
 	StrideToEnd(stride::Int, start::Int=1) = new(stride, start)
 end
+#struct StrideToEnd{D}
+#    stride::CartesianIndex{D}
+#	start::CartesianIndex{D}
+#	StrideToEnd(stride::CartesianIndex{D}, start::CartesianIndex{D}=CartesianIndex{D}(1)) where D = new(stride, start)
+#end
 import Base: to_indices, _maybetail, @_inline_meta, tail, getindex
 to_indices(A, inds, I::Tuple{StrideToEnd, Vararg{Any}})=
 	(@_inline_meta; (I[1].start:I[1].stride:inds[1][end], to_indices(A, _maybetail(inds), tail(I))...))
@@ -50,6 +64,10 @@ getindex(A::AbstractArray{T,N}, IS::IndexSubsampler{N}) where {T,N} = A[[StrideT
 function getindex(A::AbstractArray{T,N}, VW::ValueWindower{N,T}) where {T,N}
 	value_window = VW.window
 	(findfirst((value_window[1] .< arr) .| (value_window[1] .≈ arr)), findlast((value_window[2] .> arr) .| (value_window[2] .≈ arr)))
+end
+function getindex(A::AbstractArray{T,N}, VW::RightCutFrom{N,T}) where {T,N}
+	cut = VW.cut
+	StrideToEnd(1, findfirst((cut .< arr) .| (cut .≈ arr)))
 end
 
 ## RadialSlice
